301b6032276a71ab3a87da07cb97baf0d2c90884
alexzhuke <alexee_zhu@163.com>
2016-03-03 21:55:01 +0800

    improve connect

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');    //引用mongoose模块
 var config = require('./../config/config.js');
 var mongodb_uri = "mongodb://" + config.mongoDB + ":" + config.mongoPort + "/" + config.mongoCollection
-console.log(mongodb_uri);
 var db = mongoose.connect(mongodb_uri);
 
 db.connection.on("error", function (error) {
-    console.log("数据库连接失败：" + error);
+    console.log("CONNECT TO " + mongodb_uri + " FAILED: " + error);
 });
 db.connection.on("open", function () {
-    console.log("------数据库连接成功！------");
+    console.log("CONNECT TO " + mongodb_uri + " SUCCESS!" );
 });
