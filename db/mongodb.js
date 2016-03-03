c0cf614221790335e225df3737001d36f2e0ca2d
alexzhuke <alexee_zhu@163.com>
2016-03-03 22:26:36 +0800

    add default nginx config

--- db/mongodb.js
+++ db/mongodb.js
@@ -9,3 +9,20 @@ db.connection.on("error", function (error) {
 db.connection.on("open", function () {
     console.log("CONNECT TO " + mongodb_uri + " SUCCESS!" );
 });
+
+
+var ConfigSchema = new mongoose.Schema({
+    server_name : { type:String },//对应nginx default.conf中的server_name
+    listen  : { type:Number, default:80 },//对应nginx default.conf中的listen,类型为Number,默认为0
+    upstream : [{
+      server: { type:String },
+      port: { type:Number, default:80 },
+    }],
+    time : { type:Date, default:Date.now },
+    location : [{
+      path: { type:String,default:'/'},
+      proxy_pass: { type:String },
+    }]
+});
+
+var ConfigModule = db.model(config.mongoCollection, ConfigSchema);
