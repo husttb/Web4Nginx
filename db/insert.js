79a49a44665ecf2c132ddf42f93ed2058e8606d4
alexzhuke <alexee_zhu@163.com>
2016-03-07 22:12:46 +0800

    fix format error

--- db/insert.js
+++ db/insert.js
@@ -1,29 +1,30 @@
+'use strict';
 var ConfigModule = require('./mongodb.js');
 var config = require('../config/config.js');
-var moment = moment = require('moment');
+var moment = require('moment');
 var now = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
 
 var ConfigEntity = new ConfigModule({
-  "server_name": "test",
-  "listen":80,
-  "upstream":{
+  "server_name" : "test",
+  "listen" : 80,
+  "upstream" : {
     "upstream_name":"test_1",
-    "server":"192.168.7.1",
-    "port":80
+    "server" : "192.168.7.1",
+    "port" : 80
   },
-  "createTime": now,
-  "location": [
+  "createTime" : now,
+  "location" : [
   {
-    "path":"/",
-    "proxy_pass":"192,199.33.1"
+    "path" : "/",
+    "proxy_pass" : "192,199.33.1"
   }
   ]
 });
 
 ConfigEntity.save(function(error,doc){
-  if(error){
+  if(error) {
      console.log("error :" + error);
-  }else{
-     console.log( "[" + now +"]" +"INSERT SUCCESS!");
+  } else {
+     console.log("[" + now + "]" + "INSERT SUCCESS!");
   }
 });
--- routes/apis.js
+++ routes/apis.js
@@ -1,8 +1,9 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('Nginx config setting');
 });
 
